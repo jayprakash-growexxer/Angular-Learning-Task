b6a8e087a490b3ffc1c849bca461e25fa51f0902
jsatpathy-parkstreet <jsatpathy@parkstreet.com>
2026-01-09 18:29:38 +0530

    COC-114163 : Add demos for Angular Signals equality, type checking, and reactive side effects

--- frontend/src/app/demo/signal/signal.component.ts
+++ frontend/src/app/demo/signal/signal.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, signal, effect, WritableSignal, computed, Signal, untracked } from '@angular/core';
+import { Component, OnInit, signal, effect, WritableSignal, computed, Signal, untracked, isSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+interface person {
+  name: string,
+  age: number
+}
 @Component({
   selector: 'app-signal',
   standalone: true,
@@ -13,7 +18,8 @@ export class SignalComponent implements OnInit {
   constructor() {
     //  consumers : is reading the signal value
     effect(()=> {
-      console.log("Count value changed to: ", this.count());
+      // console.log("Count value changed to: ", this.count());
+      console.log("Name value changed to: ", this.person());
       // untracked() lets you read a signal without making Angular “watch” it.
       untracked(() => console.log("Double Count value is: ", this.num()));
       // console.log("Count value changed to: ", this.num);
@@ -27,6 +33,29 @@ export class SignalComponent implements OnInit {
   num:WritableSignal<number> = signal(0);
   computedNum: Signal<number> = computed(()=> this.count() * this.num());
 
+  person : WritableSignal<person[]> = signal<person[]>([], {
+    equal: (prev, curr)=> {
+      console.log('EQUAL CHECK');
+      console.log('Prev:', prev);
+      console.log('Curr:', curr);
+
+      if (prev.length !== curr.length) {
+        console.log('Length changed → update');
+        return false;
+      }
+
+      const isSame = prev.every((p, i) =>
+        p.name === curr[i].name && p.age === curr[i].age
+      );
+      console.log(isSame ? 'No real change' : 'Real change detected');
+      return isSame;
+
+      // return JSON.stringify(prev) === JSON.stringify(curr)
+    }
+  });
+  name: string = '';
+  age: number = 0;
+
   ngOnInit(): void {
   }
 
@@ -35,6 +64,23 @@ export class SignalComponent implements OnInit {
     // this._count.set(this.count() + value);  // Error: Cannot set value of a readonly signal
     this.count.update((current)=> current + value);
     // this.num += value;
+  }
+
+  updateNames(){
+    const current = this.person();
+    const newPerson = { name: this.name, age: this.age };
+
+    const exists = current.some((p)=> p.name === newPerson.name && p.age === newPerson.age);
+    console.log("Exists: ", exists);
+    if (!exists) {
+      this.person.set([...current, newPerson])
+    }
+    console.log("Current Persons: ", this.person());
+    console.log("isSignal: ", isSignal(this.name));
+    // if (!current.includes(this.name)) {
+    //   this.person.set([...current, this.name]);
+    //   this.name = '';
+    // }
   }
 
 }
